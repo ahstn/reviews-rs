c88be035b133dfb565252210a8de696e12f2c5b8
Adam Houston <phazyy9@gmail.com>
2023-05-21 20:39:12 +0100

    refactor: fixing some gpt issues

--- src/presentation/api.rs
+++ src/presentation/api.rs
@@ -1,14 +1,16 @@
-use axum::{handler::get, Router};
-use hyper::Server;
-use std::net::SocketAddr;
+use axum::{routing::get, Router};
 use tokio::net::TcpListener;
 
-pub async fn start_api_server(addr: SocketAddr) {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let server = Server::from_tcp(TcpListener::bind(addr).await.unwrap())
-        .unwrap()
-        .http(app)
-        .run();
+pub async fn start_api_server(addr: String) {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+
     println!("Listening on http://{}", addr);
-    server.await.unwrap();
+    axum::Server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+    axum::serve(listener, app).await.unwrap();
 }
